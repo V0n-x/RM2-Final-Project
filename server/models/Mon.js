996b12d6283c70a5b6a17674f8e46759a2eac8b1
V0n-x <smb1972@rit.edu>
2024-12-03 00:07:47 -0500

    FIXED MY NPM ISSUES

--- server/models/Mon.js
+++ server/models/Mon.js
@@ -4,46 +4,39 @@ const _ = require('underscore');
 const setNickname = (nickname) => _.escape(nickname).trim();
 
 const MonSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: false,
-        trim: true,
-        set: setNickname,
-      },
-    //   EVs
-    // https://mongoosejs.com/docs/schematypes.html#maps
-      efforts: {
-        type: Map,
-        of: Number,
-        required: true,
-      },
-      level: {
-        type: Number,
-        min: 1,
-        required: true,
-      },
-      owner: {
-        type: mongoose.Schema.ObjectId,
-        required: true,
-        ref: 'Account',
-      },
-      createdDate: {
-        type: Date,
-        default: Date.now,
-      },
+  name: {
+    type: String,
+    required: false,
+    trim: true,
+    set: setNickname,
+  },
+  species: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  //   EVs
+  // https://mongoosejs.com/docs/schematypes.html#maps
+  efforts: {
+    type: Map,
+    of: Number,
+    required: true,
+  },
+  level: {
+    type: Number,
+    min: 1,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const TeamSchema = new mongoose.Schema({
     mons: {
-        type: map,
-        of: [MonSchema],
+        type: [MonSchema],
         required: true,
-    },
-    size: {
-        type: Number,
-        required: true,
-        min: 1,
-        max: 6,
     },
     owner: {
         type: mongoose.Schema.ObjectId,
